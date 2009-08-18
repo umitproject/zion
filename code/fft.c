20057641549ada2d5acec999ec3dfaca27455846
Joao Paulo de Souza Medeiros <ignotus@umitproject.org>
2009-08-18 18:30:29 +0000

    Changed 2008 copyright.
    git-svn-id: http://svn.umitproject.org/svnroot/umit/branch/clann@5307 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- code/fft.c
+++ code/fft.c
@@ -1,6 +1,5 @@
 /**
- * Copyright (C) 2009 Joao Paulo de Souza Medeiros
- * Copyright (C) 2009 Adriano Monteiro Marques
+ * Copyright (C) 2008-2009 Adriano Monteiro Marques
  *
  * Author(s): Joao Paulo de Souza Medeiros <ignotus21@gmail.com>
  *
